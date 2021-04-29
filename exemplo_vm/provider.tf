fc6e90662532a3c06ea67618f521cf63d6a7e5aa
diegobenelli <diego.benelli@hotmail.com>
2021-04-29 00:24:40 +0000

    Aula Terraform Leo

---
+++ exemplo_vm/provider.tf
@@ -0,0 +1,6 @@
+provider "google" {
+  project = "diegogouvea-mbaimpacta-turma04"
+  region = "us-central1"
+  zone = "us-central1-a"
+  credentials = "../../diegogouvea-mbaimpacta-turma04-d63f6a63566e.json"
+}
